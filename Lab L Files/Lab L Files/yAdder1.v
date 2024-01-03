605971fd357ec91bafcfabd59620c4264f6146ad
Nikhil Arora <155202181+nikhilaroraa@users.noreply.github.com>
2024-01-03 15:15:11 -0500

    Delete Lab L Files/Lab L Files directory

--- Lab L Files/Lab L Files/yAdder1.v
+++
@@ -1,9 +0,0 @@
-module yAdder1(z, cout, a, b, cin);
-    output z, cout;
-    input a, b, cin;
-    xor left_xor(tmp, a, b);
-    xor right_xor(z, cin, tmp);
-    and left_and(outL, a, b);
-    and right_and(outR, tmp, cin);
-    or my_or(cout, outR, outL);
-endmodule
